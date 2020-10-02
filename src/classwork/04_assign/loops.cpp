95682158cdff9fbfd167d82389f41c4ee705ede1
ngialusis <youremail@example.com>
2020-10-01 19:17:10 -0500

    Homework 3 and some Test case detection fixes

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,13 @@
 //cpp
+int factorial(int num)
+{
+    int inc = num-1;
+    int sum = num;
+    while(inc>1)
+    {
+        sum=sum * (inc);
+        inc--;
+    }
+
+    return sum;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
-
+#include<string>
+#include<iostream>
+#include "loops.h"
 //write using statements for cin and cout
-
+using std::cin; using std::cout;
 
 /*
 Use a do while loop to prompt the user for 
@@ -9,7 +11,25 @@ a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
-{
-
-	return 0;
+{	
+	int fact=0;
+	bool y = true;
+	do
+	{
+		cout<<"Provide an integer 1-20. \n";
+		cin>>fact;
+		if(fact<=20 && fact>=1)
+		{
+			y=false;
+		}
+		else
+		{
+			cout<<"Number not valid. \n";
+		}
+		
+		
+	} while (y);
+	int ans = factorial(fact);	
+	cout<<ans<<"\n";
+	return ans;
 }
